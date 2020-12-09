0b48a22a2a7dc9eb882168fb21b553e23f142d4f
b h <bh@ubuntutest.com>
2020-12-08 23:07:30 -0500

    First

---
+++ src/com/hbtest/Main.java
@@ -0,0 +1,9 @@
+package com.hbtest;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.print("First " + 5 + " Hello world");
+    }
+}
